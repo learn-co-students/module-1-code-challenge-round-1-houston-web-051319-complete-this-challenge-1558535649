d3120532245fd2252b6589b916125ed227147141
Maruf <marufbinawal@gmail.com>
2019-05-22 10:50:03 -0500

    done

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,8 +1,36 @@
 class Restaurant
   attr_reader :name
 
+  @@all = []
+
+  def self.all
+    @@all
+end
+
   def initialize(name)
     @name = name
+    @@all << self
+  end
+
+  
+  def reviews
+    Review.all.select do | customer |
+      customer.review == self
+    end
+  end
+
+  def customers
+    self.reviews.map do | review |
+      review.customer
+      end
+  end
+
+  def averge_start_rating
+    Review.star_rating.average
+  end
+
+  def longest_review
+    review.max_by(content:length)
   end
 
 end
--- app/models/review.rb
+++ app/models/review.rb
@@ -1,4 +1,28 @@
 class Review
-  
+    attr_accessor :customer, :restaurant, :star_rating, :content
+    
+
+    @@all = []
+    def self.all
+        @@all
+    end
+
+    def initialize(customer, restaurant, star_rating, content)
+        @customer = customer
+        @restaurant = restaurant
+        @star_rating = star_rating
+        @content = content
+        @@all << self
+      
+    end
+
+    def rating
+        star_rating.rand(1..5)
+    end
+
+    def content =(content)
+        @content = cotent
+    end
+    
 end
 
