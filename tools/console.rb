4b2fa7f283715b9464848a16847b42ccb5cffc2b
Carlos Barrera <barrera9696@gmail.com>
2019-05-22 10:47:04 -0500

    done

--- tools/console.rb
+++ tools/console.rb
@@ -7,5 +7,23 @@ end
 # This is a convenient place to define variables and/or set up new object instances,
 # so they will be available to test and play around with in your console
 
+# Restaurant 
+five_guys = Restaurant.new("Five Guys")                          #:customer, :restaurant, :rating, :content
+waffle_house = Restaurant.new("Waffle House")
+ihop = Restaurant.new("IHOP")
+
+# Customer
+sally_smith = Customer.new("Sally", "Smith")
+john_toby = Customer.new("John", "Toby")
+eva_johnson = Customer.new("Eva", "Johnson")
+
+
+# Review 
+sally_review = Review.new(sally_smith, five_guys, 4, "Best Burger")
+john_review = Review.new(john_toby, waffle_house, 5, "Best Waffle")
+eva_review = Review.new(eva_johnson, ihop, 5, "Best Pancakes")
+
+
+
 binding.pry
 0 #leave this here to ensure binding.pry isn't the last line
