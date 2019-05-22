d8e5dfd4a5e2527c5fd39fde7c4ff7111d693e75
feven tsegay <feventsegay6@gmail.com>
2019-05-22 10:51:14 -0500

    done

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -1,12 +1,44 @@
 class Customer
+  @@all=[]
   attr_reader :first_name, :last_name
 
   def initialize(first_name, last_name)
     @first_name = first_name
     @last_name  = last_name
+    @@all << self
   end
 
   def full_name
     "#{first_name} #{last_name}"
   end
+
+  def add_review(restaurant,content,rating)
+    Review.new(restaurant,content,rating,self)
+      
+  end
+  def num_reviews
+    Review.all.select do |reviews|
+      reviews.customer == self
+     end
+  end
+
+  def restaurants
+     self.num_reviews.map do|restaurants|
+       restaurants.restaurant
+       end
+  end
+
+
+
+  def self.all
+    @@all
+  end
+
+
+  def self.find_by_name(name)
+    Customer.all.select do |full_name|
+      full_name.downcase == full_name.downcase
+          end
+     end
+
 end
