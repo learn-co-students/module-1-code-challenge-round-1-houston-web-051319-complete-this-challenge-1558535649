ca3b7d5b106f8a97f42ad09167cedb053e96f5e3
Hermann Krohn <krohnhermannacosta@gmail.com>
2019-05-22 10:46:14 -0500

    Hermann's Solution.

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,8 +1,62 @@
 class Restaurant
   attr_reader :name
 
+  @@all = []
   def initialize(name)
     @name = name
+    @@all << self
   end
 
+  def self.all 
+    @@all
+  end
+
+  def customers
+    output_array = []
+    Review.all.select do |review|
+      review.restaurant == self
+    end.uniq do |review|
+      review.customer
+    end.each do |review|
+      output_array << review.customer
+    end
+    output_array
+  end
+
+  def reviews
+    Review.all.select do |review|
+      review.restaurant == self
+    end
+  end
+
+  def average_star_rating
+    average = 0
+    star_sum = 0
+    review_array = self.reviews
+    review_array.each do |review|
+      star_sum += review.star_rating 
+    end
+    average = star_sum/review_array.length
+    average
+  end
+
+  def longest_review
+    review_array = self.reviews
+    longest_review = Review.all[0]
+    longest_review_length = longest_review.content.length
+    review_array.each do |review|
+      current_review_length = review.content.length
+      if current_review_length > longest_review_length
+        longest_review = review
+        longest_review_length = longest_review.content.length
+      end
+    end
+    longest_review.content
+  end
+
+  def self.find_by_name(name)
+    Restaurant.all.find do |restaurant|
+      restaurant.name == name
+    end
+  end        
 end
--- app/models/review.rb
+++ app/models/review.rb
@@ -1,4 +1,24 @@
 class Review
   
+    attr_reader :customer, :restaurant, :star_rating, :content
+
+    @@all = []
+
+    def initialize(customer, restaurant, star_rating, content)
+        @customer = customer
+        @restaurant = restaurant
+        if(star_rating < 1 || star_rating > 5)
+            @star_rating = 1
+        else
+            @star_rating = star_rating
+        end
+        @content = content
+        @@all << self
+    end
+
+    def self.all
+        @@all
+    end
+
 end
 
