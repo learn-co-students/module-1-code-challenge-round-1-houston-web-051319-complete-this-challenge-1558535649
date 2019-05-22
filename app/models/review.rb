701869edfb188fc31b2d25993edf332651036812
BobNarwhalKing <danceflurry4366@gmail.com>
2019-05-22 10:43:19 -0500

    done

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,4 +1,25 @@
 class Review
+
+    attr_reader :rating, :content, :customer, :restaurant
+
+    @@all = []
+
+    def initialize (rating, content, customer, restaurant)
+        @rating = rating
+        @content = content
+        @customer = customer
+        @restaurant = restaurant
+        @@all << self
+    end
+
+    def self.all
+        @@all
+    end
+
+    def content_length
+        split = self.content.split(//)
+        split.length
+    end
   
 end
 
